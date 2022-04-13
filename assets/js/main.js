a7a5a3b097a873eebd700c4cf80a96513efdc279
r0ck70 <itzmonir@gmail.com>
2022-04-14 03:47:11 +0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Up Button on Scroll
+const element = document.querySelector('.up-button');
+
+function onScroll(event) {
+    const current = document.documentElement.scrollTop;
+    const maxHeight = document.body.scrollHeight;
+
+    // if current position is more than 80% of document height
+    if (current > maxHeight * 0.2) {
+        element.classList.add('is-active');
+    } else {
+        element.classList.remove('is-active');
+    }
+}
+
+window.addEventListener('scroll', event => onScroll(event));
+
+// Go to Top
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
